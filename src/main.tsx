a0f6a62bb43a75c8a58d4f4732a6248668c841fa
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 15:07:23 -0400

    Use assertion to avoid nonnull

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router.tsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
     <Router />
   </StrictMode>
