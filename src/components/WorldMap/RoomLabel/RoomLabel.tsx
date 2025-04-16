801218dcd0c3042041ea7b8b0fda5895a11b9326
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 15:49:51 -0400

    Fix missing dep and strict error

--- src/components/WorldMap/RoomLabel/RoomLabel.tsx
+++ src/components/WorldMap/RoomLabel/RoomLabel.tsx
@@ -1,28 +1,28 @@
-import { useMemo } from 'react'
-import { calculateCentroid } from '../geometryHelpers'
-import InfoLines from './InfoLines'
-import RoomTitle from './RoomTitle'
-import type { CombinedRoomData } from '@/lib/hooks/useCombinedData'
+import { useMemo } from "react";
+import { calculateCentroid } from "../geometryHelpers";
+import InfoLines from "./InfoLines";
+import RoomTitle from "./RoomTitle";
+import type { CombinedRoomData } from "@/lib/hooks/useCombinedData";
 
 type RoomLabelProps = {
-  roomData: CombinedRoomData
-}
+  roomData: CombinedRoomData;
+};
 
 export default function RoomLabel({ roomData }: RoomLabelProps) {
-  const { name, id, originOffset, points, labelOffset } = roomData
+  const { name, id, originOffset, points, labelOffset } = roomData;
 
   // Calculate lable position
   const adjustedPoints: Array<[number, number]> = useMemo(() => {
     return points.map(([x, y]) => [
       x + originOffset[0],
       (y + originOffset[1]) * -1, // Y axis increases in downward direction
-    ])
-  }, [points, originOffset])
+    ]);
+  }, [points, originOffset]);
 
   const defaultLabelPosition = useMemo(
     () => calculateCentroid(adjustedPoints),
-    [adjustedPoints],
-  )
+    [adjustedPoints]
+  );
 
   const labelPosition: [number, number] = useMemo(
     () =>
@@ -32,22 +32,22 @@ export default function RoomLabel({ roomData }: RoomLabelProps) {
             (originOffset[1] + labelOffset[1]) * -1, // Y axis increases in downward direction
           ]
         : defaultLabelPosition,
-    [labelOffset, defaultLabelPosition],
-  )
+    [labelOffset, originOffset, defaultLabelPosition]
+  );
 
-  const labelX = labelPosition[0]
-  const labelY = labelPosition[1]
-  const labelZ = 0.2
+  const labelX = labelPosition[0];
+  const labelY = labelPosition[1];
+  const labelZ = 0.2;
 
   return (
     <>
       <group
-        key={`${name}-${id}-label-group`}
+        key={`${name}-${id.toString()}-label-group`}
         position={[labelX, labelY, labelZ]}
       >
         <RoomTitle name={name} />
         <InfoLines roomData={roomData} />
       </group>
     </>
-  )
+  );
 }
