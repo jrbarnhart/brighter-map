3f23ebedc76b200f271b964d11980100d5fd9ee1
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 16:24:58 -0400

    Add conditional rendering based on query state

--- src/routes/root/root.route.tsx
+++ src/routes/root/root.route.tsx
@@ -1,16 +1,41 @@
-import { Link, Outlet } from "react-router";
+import FiltersPanel from "@/components/FiltersPanel/FiltersPanel";
+import InfoPanel from "@/components/InfoPanel/InfoPanel";
+import MapControls from "@/components/MapControls/MapControls";
+import WorldMap from "@/components/WorldMap/WorldMap";
+// import useLazyQueries from "@/lib/hooks/useLazyQueries";
+import { baseMapDataQueryOptions } from "@/queries/baseMapData/baseMapDataQueryOptions";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 function RootRoute() {
+  // Base map data comes from loader
+  const { data, isLoading, isError } = useQuery(baseMapDataQueryOptions);
+
+  // Side panel state
+  const [filtersOpen, setFiltersOpen] = useState(false);
+  const [infoOpen, setInfoOpen] = useState(false);
+
+  // Lazy data loading
+  //const { data: lazyData, handlers: lazyHandlers } = useLazyQueries();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (isError || !data) return <p>Error!</p>;
+
   return (
-    <div className="h-full w-full flex">
-      <div className="h-full w-1/2 bg-amber-500 flex flex-col items-center justify-center">
-        <p>Canvas</p>
-        <Link to="/monsters">Monsters</Link>
-        <Link to="/resources">Resources</Link>
+    <div
+      id="app-container"
+      className="relative h-svh w-svw flex overflow-hidden"
+    >
+      <FiltersPanel open={filtersOpen} setOpen={setFiltersOpen} />
+      <div className="w-full absolute top-0 right-0 z-10 p-8">
+        <MapControls
+          setFiltersOpen={setFiltersOpen}
+          setInfoOpen={setInfoOpen}
+        />
       </div>
-      <div className="h-full w-1/2 bg-purple-400">
-        <Outlet />
-      </div>
+      <WorldMap baseMapData={data} />
+      <InfoPanel open={infoOpen} setOpen={setInfoOpen} />
     </div>
   );
 }
