e300d71186471601fd53eceb5068854d6cda018a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 17:00:32 -0400

    Add test links

--- src/components/FiltersPanel/FiltersPanel.tsx
+++ src/components/FiltersPanel/FiltersPanel.tsx
@@ -1,35 +1,39 @@
-import { ArrowLeftToLine } from 'lucide-react'
-import { Button } from '../ui/button'
-import type { SetStateAction } from 'react'
-import { cn } from '@/lib/utils'
+import { ArrowLeftToLine } from "lucide-react";
+import { Button } from "../ui/button";
+import type { SetStateAction } from "react";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router";
 
 type FiltersPanelProps = {
-  open: boolean
-  setOpen: React.Dispatch<SetStateAction<boolean>>
-}
+  open: boolean;
+  setOpen: React.Dispatch<SetStateAction<boolean>>;
+};
 
 export default function FiltersPanel({ open, setOpen }: FiltersPanelProps) {
   return (
     <aside
       className={cn(
-        'flex flex-col gap-3 p-6 w-full shrink-0 transition-transform duration-300 ease-in-out z-20',
-        'absolute left-0 top-0 bottom-0 right-0 bg-background border-r', // Mobile
-        open ? 'translate-x-0' : '-translate-x-full', // Translate if open
-        'md:w-60', // Desktop
+        "flex flex-col gap-3 p-6 w-full shrink-0 transition-transform duration-300 ease-in-out z-20",
+        "absolute left-0 top-0 bottom-0 right-0 bg-background border-r", // Mobile
+        open ? "translate-x-0" : "-translate-x-full", // Translate if open
+        "md:w-60" // Desktop
       )}
     >
       <Button
         className="w-10 h-10 md:w-12 md:h-12 cursor-pointer bg-gray-200 hover:bg-gray-300"
         onClick={() => {
-          setOpen(false)
+          setOpen(false);
         }}
         inert={!open ? true : undefined}
       >
         <ArrowLeftToLine />
       </Button>
+      <Link to="/monsters">Monsters</Link>
+      <Link to="/resources">Resources</Link>
+      <Link to="/">Home</Link>
       <p>Filter Names</p>
       <p>Filter Monsters</p>
       <p>Filter Resources</p>
     </aside>
-  )
+  );
 }
