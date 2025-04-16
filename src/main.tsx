1b4d0b7493a80cbc3815dd187b43ab3226caefe0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 15:19:13 -0400

    Rename file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import Router from "./routes/Router.tsx";
+import Router from "./routes/router";
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
