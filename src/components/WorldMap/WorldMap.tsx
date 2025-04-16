8a0b6f45b837074d26bd9301d4ca09883df4c97e
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-16 15:50:17 -0400

    Fix strict errors

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,24 +1,31 @@
-import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import React from 'react'
-import RoomShape from './RoomShape/RoomShape'
-import RoomLabel from './RoomLabel/RoomLabel'
-import type { BaseMapData } from '@/queries/baseMapData/baseMapData'
-import useCombinedData from '@/lib/hooks/useCombinedData'
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import React from "react";
+import RoomShape from "./RoomShape/RoomShape";
+import RoomLabel from "./RoomLabel/RoomLabel";
+import type { BaseMapData } from "@/queries/baseMapData/baseMapData";
+import useCombinedData from "@/lib/hooks/useCombinedData";
 
 type WorldMapProps = {
-  baseMapData: BaseMapData
-}
+  baseMapData: BaseMapData;
+};
 
 function Controls() {
-  const { invalidate } = useThree()
-  return <OrbitControls enableRotate={false} onChange={() => invalidate()} />
+  const { invalidate } = useThree();
+  return (
+    <OrbitControls
+      enableRotate={false}
+      onChange={() => {
+        invalidate();
+      }}
+    />
+  );
 }
 
 export default function WorldMap({ ...props }: WorldMapProps) {
-  const { baseMapData } = props
+  const { baseMapData } = props;
 
-  const combinedRoomData = useCombinedData({ baseMapData })
+  const combinedRoomData = useCombinedData({ baseMapData });
 
   return (
     <div id="canvas-container" className="h-full w-full">
@@ -29,12 +36,12 @@ export default function WorldMap({ ...props }: WorldMapProps) {
       >
         <Controls />
         {combinedRoomData.map((roomData) => (
-          <React.Fragment key={`${roomData.name}-${roomData.id}`}>
+          <React.Fragment key={`${roomData.name}-${roomData.id.toString()}`}>
             <RoomShape roomData={roomData} />
             <RoomLabel roomData={roomData} />
           </React.Fragment>
         ))}
       </Canvas>
     </div>
-  )
+  );
 }
