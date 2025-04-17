8385aa516861c74915e40e89dbe8ed32fe0e677a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-17 17:08:35 -0400

    Add basic monster details component

---
+++ src/components/InfoPanel/MonsterDetails/MonsterDetails.tsx
@@ -0,0 +1,61 @@
+import {
+  Arborae,
+  Cryonae,
+  Impact,
+  Infernae,
+  Necromae,
+  Tempestae,
+} from "@/assets/gameIcons";
+import { components } from "@/lib/types/apiTypes";
+import { MonsterByIdQueryOptions } from "@/queries/monsters/monstersQueryOptions";
+import { useQuery } from "@tanstack/react-query";
+import { X } from "lucide-react";
+import { useParams } from "react-router";
+
+export default function MonsterDetails() {
+  const { id } = useParams();
+  const { data, isLoading, error } = useQuery(
+    MonsterByIdQueryOptions(id || -1)
+  ); // -1 b/c all real ids are +
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading room data...</p>
+      </div>
+    );
+  }
+
+  if (error || !data) {
+    return (
+      <div>
+        <p>There was an error while fetching the data.</p>
+      </div>
+    );
+  }
+
+  type IconMap = {
+    [key in components["schemas"]["AttackElementsEnum"]["value"]]: React.ReactNode;
+  };
+
+  const iconMap: IconMap = {
+    ARBORAE: <Arborae />,
+    CRYONAE: <Cryonae />,
+    INFERNAE: <Infernae />,
+    NECROMAE: <Necromae />,
+    TEMPESTAE: <Tempestae />,
+    IMPACT: <Impact />,
+    NONE: <X />,
+  };
+
+  return (
+    <div className="text-white">
+      <h2 className="font-bold text-2xl">{data.name}</h2>
+      <h3>Region: {data.region.name}</h3>
+      <div className="flex items-center">
+        <p>Attacks: {data.attackElement}</p>
+        {iconMap[data.attackElement]}
+      </div>
+    </div>
+  );
+}
