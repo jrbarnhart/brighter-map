9c61143467fd6d1019da60be35011dc15bf0067b
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-17 10:05:09 -0400

    Toggle names based on state

--- src/components/WorldMap/RoomLabel/RoomTitle.tsx
+++ src/components/WorldMap/RoomLabel/RoomTitle.tsx
@@ -1,43 +1,47 @@
-import { Text } from '@react-three/drei'
-import { useMemo } from 'react'
+import { Text } from "@react-three/drei";
+import { useMemo } from "react";
 
 type RoomTitleProps = {
-  name: string
-}
+  name: string;
+  showNames: boolean;
+};
 
-export default function RoomTitle({ name }: RoomTitleProps) {
+export default function RoomTitle({ name, showNames }: RoomTitleProps) {
   // Config Constants
-  const OFFSET_Y = 0.2
-  const BG_Z = 0
-  const TEXT_Z = 0.01
-  const FONT_HEIGHT = 0.5
-  const AVERAGE_FONT_WIDTH = 0.6
-  const PADDING_Y = 0.2
-  const PADDING_X = 0.2
+  const OFFSET_Y = 0.2;
+  const BG_Z = 0;
+  const TEXT_Z = 0.01;
+  const FONT_HEIGHT = 0.5;
+  const AVERAGE_FONT_WIDTH = 0.6;
+  const PADDING_Y = 0.2;
+  const PADDING_X = 0.2;
 
   // Estimate background size
   const bgDimensions = useMemo(() => {
-    if (name.length === 0) return { width: 0, height: 0 }
+    if (name.length === 0) return { width: 0, height: 0 };
 
-    const lineLength = name.length
+    const lineLength = name.length;
 
-    const paddingVertical = PADDING_Y * 2 // Top and bottom padding
-    const height = FONT_HEIGHT + paddingVertical
+    const paddingVertical = PADDING_Y * 2; // Top and bottom padding
+    const height = FONT_HEIGHT + paddingVertical;
 
-    const paddingHorizontal = PADDING_X * 2 // Left and right padding (adjust as needed)
+    const paddingHorizontal = PADDING_X * 2; // Left and right padding (adjust as needed)
     const width =
-      lineLength * FONT_HEIGHT * AVERAGE_FONT_WIDTH + paddingHorizontal
+      lineLength * FONT_HEIGHT * AVERAGE_FONT_WIDTH + paddingHorizontal;
 
     return {
       width: width,
       height: height,
-    }
-  }, [name])
+    };
+  }, [name]);
 
   return (
     <>
       {/* Title Background */}
-      <mesh position={[0, -bgDimensions.height / 2 + OFFSET_Y, BG_Z]}>
+      <mesh
+        position={[0, -bgDimensions.height / 2 + OFFSET_Y, BG_Z]}
+        visible={showNames}
+      >
         <planeGeometry args={[bgDimensions.width, bgDimensions.height]} />
         <meshBasicMaterial color="white" opacity={1} />
       </mesh>
@@ -49,9 +53,10 @@ export default function RoomTitle({ name }: RoomTitleProps) {
         anchorX="center"
         anchorY="top"
         fontWeight="bold"
+        visible={showNames}
       >
         {name}
       </Text>
     </>
-  )
+  );
 }
