2e277ddefab467323947871bdfd85af7c9d7105d
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-17 17:23:06 -0400

    Add info links for rooms monster is in

--- src/components/InfoPanel/MonsterDetails/MonsterDetails.tsx
+++ src/components/InfoPanel/MonsterDetails/MonsterDetails.tsx
@@ -11,6 +11,7 @@ import { MonsterByIdQueryOptions } from "@/queries/monsters/monstersQueryOptions
 import { useQuery } from "@tanstack/react-query";
 import { X } from "lucide-react";
 import { useParams } from "react-router";
+import InfoLink from "../InfoLink/InfoLink";
 
 export default function MonsterDetails() {
   const { id } = useParams();
@@ -52,6 +53,17 @@ export default function MonsterDetails() {
     <div className="text-white">
       <h2 className="font-bold text-2xl">{data.name}</h2>
       <h3>Region: {data.region.name}</h3>
+      <div>
+        {data.rooms.map((r) => (
+          <InfoLink
+            to={`/rooms/${r.id.toString()}`}
+            key={`${r.name}-${r.id.toString()}`}
+            variant="room"
+          >
+            {r.name}
+          </InfoLink>
+        ))}
+      </div>
       <div className="flex items-center">
         <p>Attacks: {data.attackElement}</p>
         {iconMap[data.attackElement]}
