02505dc23eadc7349425c7d1b04642c55d7134fa
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-17 13:01:50 -0400

    Add route for room details

--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootRoute from "./root/root.route";
+import RoomDetails from "@/components/InfoPanel/RoomDetails/RoomDetails";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
         <Route path="/" element={<RootRoute />}>
           <Route path="/monsters" element={<div>Monster info</div>} />
           <Route path="/resources" element={<div>Resource info</div>} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
