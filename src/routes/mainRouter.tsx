025c66deb1dc915b923dc810ef23de9d75e4fafe
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-17 17:09:14 -0400

    Add route for monster details

--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootRoute from "./root/root.route";
 import RoomDetails from "@/components/InfoPanel/RoomDetails/RoomDetails";
+import MonsterDetails from "@/components/InfoPanel/MonsterDetails/MonsterDetails";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
           <Route path="/monsters" element={<div>Monster info</div>} />
           <Route path="/resources" element={<div>Resource info</div>} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/monsters/:id" element={<MonsterDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
