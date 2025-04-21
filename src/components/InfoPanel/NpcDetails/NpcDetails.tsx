9f93cbb1a5397b2c8264f2555e27405dfc394db7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-21 18:02:20 -0400

    Add npc details content

--- src/components/InfoPanel/NpcDetails/NpcDetails.tsx
+++ src/components/InfoPanel/NpcDetails/NpcDetails.tsx
@@ -3,6 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import InfoContainer from "../infoContents/InfoContainer";
 import InfoTitle from "../infoContents/InfoTitle";
+import InfoLabel from "../infoContents/InfoLabel";
+import React from "react";
+import InfoLink from "../InfoLink/InfoLink";
 
 export default function NpcDetails() {
   const { id } = useParams();
@@ -29,6 +32,33 @@ export default function NpcDetails() {
   return (
     <InfoContainer>
       <InfoTitle>{data.name}</InfoTitle>
+      {data.rooms.length > 0 && <InfoLabel>Rooms:</InfoLabel>}
+      {data.rooms.map((r, index) => (
+        <React.Fragment key={`${r.name}-${r.id.toString()}`}>
+          <InfoLink to={`/rooms/${r.id.toString()}`} variant="room">
+            {r.name}
+          </InfoLink>
+          {index !== data.rooms.length - 1 && ", "}
+        </React.Fragment>
+      ))}
+      {data.questSteps.length > 0 && <InfoLabel>Quest Steps</InfoLabel>}
+      {data.questSteps.map((s) => (
+        <p key={`${s.questId.toString()}-${s.id.toString()}`}>
+          {s.description} - Quest Name{" "}
+          {/* TODO: Replace with actual quest name after API update */}
+        </p>
+      ))}
+      {data.vendor && (
+        <>
+          <InfoLabel>Vendor for:</InfoLabel>
+          <InfoLink
+            to={`/vendors/${data.vendor.id.toString()}`}
+            variant="vendor"
+          >
+            {data.vendor.name}
+          </InfoLink>
+        </>
+      )}
     </InfoContainer>
   );
 }
