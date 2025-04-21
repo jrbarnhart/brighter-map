4bf32ca5d76ae10ad2737b35d3cb42c1eadecb96
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-21 19:21:00 -0400

    Add content for resource details

--- src/routes/ResourceDetails/ResourceDetails.tsx
+++ src/routes/ResourceDetails/ResourceDetails.tsx
@@ -1,7 +1,11 @@
+import { Passive } from "@/assets/gameIcons";
 import InfoContainer from "@/components/InfoPanel/infoContents/InfoContainer";
+import InfoLabel from "@/components/InfoPanel/infoContents/InfoLabel";
 import InfoTitle from "@/components/InfoPanel/infoContents/InfoTitle";
+import InfoLink from "@/components/InfoPanel/InfoLink/InfoLink";
 import { resourceByIdQueryOptions } from "@/queries/resources/resourcesQueryOptions";
 import { useQuery } from "@tanstack/react-query";
+import React from "react";
 import { useParams } from "react-router";
 
 export default function ResourceDetails() {
@@ -29,6 +33,35 @@ export default function ResourceDetails() {
   return (
     <InfoContainer>
       <InfoTitle>{data.name}</InfoTitle>
+
+      {data.passive && (
+        <div className="flex items-center gap-3 h-8">
+          <InfoLabel>Passive </InfoLabel>
+          <Passive />
+        </div>
+      )}
+
+      <div className="flex items-baseline gap-2">
+        <InfoLabel>Skill:</InfoLabel>
+        <p>{data.skill.name}</p>
+      </div>
+      {data.rooms.length > 0 && <InfoLabel>Rooms:</InfoLabel>}
+      {data.rooms.map((r, index) => (
+        <React.Fragment key={`${r.name}-${r.id.toString()}`}>
+          <InfoLink to={`/rooms/${r.id.toString()}`} variant="room">
+            {r.name}
+          </InfoLink>
+          {index !== data.rooms.length - 1 && ", "}
+        </React.Fragment>
+      ))}
+      <InfoLabel>Variants:</InfoLabel>
+      {data.variants.map((v) => (
+        <React.Fragment key={`${v.name}-${v.id.toString()}`}>
+          <p>
+            {v.name} {data.name}
+          </p>
+        </React.Fragment>
+      ))}
     </InfoContainer>
   );
 }
