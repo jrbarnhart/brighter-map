3d5fb73efa151a313e98718203d8aedfb8949964
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-21 17:24:06 -0400

    Map vendor data to info elements

--- src/components/InfoPanel/VendorDetails/VendorDetails.tsx
+++ src/components/InfoPanel/VendorDetails/VendorDetails.tsx
@@ -1,6 +1,10 @@
 import { vendorByIdQueryOptions } from "@/queries/vendors/vendorsQueryOptions";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
+import InfoContainer from "../infoContents/InfoContainer";
+import InfoTitle from "../infoContents/InfoTitle";
+import InfoLabel from "../infoContents/InfoLabel";
+import React from "react";
 
 export default function VendorDetails() {
   const { id } = useParams();
@@ -23,4 +27,57 @@ export default function VendorDetails() {
       </div>
     );
   }
+
+  return (
+    <InfoContainer>
+      <InfoTitle>{data.name || data.npc.name}</InfoTitle>
+      <div className="flex items-baseline gap-2">
+        <InfoLabel>Npc:</InfoLabel>
+        <p>{data.npc.name}</p>
+      </div>
+      <InfoLabel>Stock:</InfoLabel>
+      {data.miscItems.length > 0 && <InfoLabel>Misc Items:</InfoLabel>}
+      {data.miscItems.map((i, index) => (
+        <React.Fragment
+          key={`misc-item-${i.id.toString()}-${index.toString()}`}
+        >
+          <p>{i.name}</p>
+        </React.Fragment>
+      ))}
+      {data.consumableVariants.length > 0 && (
+        <InfoLabel>Consumables:</InfoLabel>
+      )}
+      {data.consumableVariants.map((c, index) => (
+        <React.Fragment
+          key={`consumable-variant-${c.id.toString()}-${index.toString()}`}
+        >
+          <p>{c.name}</p>
+        </React.Fragment>
+      ))}
+      {data.armorVariants.length > 0 && <InfoLabel>Armor:</InfoLabel>}
+      {data.armorVariants.map((a, index) => (
+        <React.Fragment
+          key={`armor-variant-${a.id.toString()}-${index.toString()}`}
+        >
+          <p>{a.name}</p>
+        </React.Fragment>
+      ))}
+      {data.weaponVariants.length > 0 && <InfoLabel>Weapons:</InfoLabel>}
+      {data.weaponVariants.map((w, index) => (
+        <React.Fragment
+          key={`weapon-variants-${w.id.toString()}-${index.toString()}`}
+        >
+          <p>{w.name}</p>
+        </React.Fragment>
+      ))}
+      {data.resourceVariants.length > 0 && <InfoLabel>Resources:</InfoLabel>}
+      {data.resourceVariants.map((r, index) => (
+        <React.Fragment
+          key={`resource-variant-${r.id.toString()}-${index.toString()}`}
+        >
+          <p>{r.name}</p>
+        </React.Fragment>
+      ))}
+    </InfoContainer>
+  );
 }
