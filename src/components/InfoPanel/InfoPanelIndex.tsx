d57cdd2afe646d28960744a4329246fce9d47fea
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-21 14:57:14 -0400

    Add info panel index content

---
+++ src/components/InfoPanel/InfoPanelIndex.tsx
@@ -0,0 +1,9 @@
+export default function InfoPanelIndex() {
+  return (
+    <div className="text-white flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Brighter Map</h1>
+      <p>Click or tap a room to see info about that room's contents.</p>
+      <p>Looking for something specific? Try searching for it.</p>
+    </div>
+  );
+}
--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import RootRoute from "./root/root.route";
 import RoomDetails from "@/components/InfoPanel/RoomDetails/RoomDetails";
 import MonsterDetails from "@/components/InfoPanel/MonsterDetails/MonsterDetails";
+import InfoPanelIndex from "@/components/InfoPanel/InfoPanelIndex";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootRoute />}>
+          <Route index element={<InfoPanelIndex />} />
           <Route path="/monsters" element={<div>Monster info</div>} />
           <Route path="/resources" element={<div>Resource info</div>} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
