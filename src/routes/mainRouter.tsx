f1fb009ab775d6c964f2711c3f6a6e3f0bd88f10
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-21 16:51:53 -0400

    Add route for vendor details

--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -3,6 +3,7 @@ import RootRoute from "./root/root.route";
 import RoomDetails from "@/components/InfoPanel/RoomDetails/RoomDetails";
 import MonsterDetails from "@/components/InfoPanel/MonsterDetails/MonsterDetails";
 import InfoPanelIndex from "@/components/InfoPanel/InfoPanelIndex";
+import VendorDetails from "@/components/InfoPanel/VendorDetails/VendorDetails";
 
 export default function Router() {
   return (
@@ -14,6 +15,7 @@ export default function Router() {
           <Route path="/resources" element={<div>Resource info</div>} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/monsters/:id" element={<MonsterDetails />} />
+          <Route path="/vendors/:id" element={<VendorDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
