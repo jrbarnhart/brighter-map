3eaf477028a890fd48ddf7025b26fa2af6d80c4a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-27 23:09:11 -0400

    Use extended search data type in searchable item type

--- src/lib/types/searchTypes.ts
+++ src/lib/types/searchTypes.ts
@@ -2,10 +2,9 @@ import { components } from "./apiTypes";
 
 export type SearchDataType = "monster" | "room" | "vendor" | "npc" | "resource";
 
-export type ExtendedSearchData = { url: string };
+export type ExtendedSearchData = { url: string; type: SearchDataType };
 
-export type SearchableItem = components["schemas"]["RoomEntity"] & {
-  url: string;
-};
+export type SearchableItem = components["schemas"]["RoomEntity"] &
+  ExtendedSearchData;
 
 export type SearchData = SearchableItem[];
