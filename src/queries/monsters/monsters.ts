f9e195ce2dcbcf32ccd7704109a407c0fec00dd7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-04-27 21:41:57 -0400

    Add query for all monsters

--- src/queries/monsters/monsters.ts
+++ src/queries/monsters/monsters.ts
@@ -2,6 +2,24 @@ import axios from "axios";
 import { axiosClient } from "../axiosClient";
 import type { paths } from "@/lib/types/apiTypes";
 
+export async function fetchMonsters() {
+  // console.info('Fetching monsters');
+  try {
+    const monsterQuery = await axiosClient.get<
+      paths["/monsters"]["get"]["responses"]["200"]["content"]["application/json"]
+    >(`/monsters`);
+
+    return monsterQuery.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.message);
+    } else {
+      console.error(error);
+      throw new Error("An unexpected error occurred while fetching monsters.");
+    }
+  }
+}
+
 export async function fetchMonsterById({ id }: { id: string | number }) {
   // console.info('Fetching monster by id...');
   try {
--- src/queries/monsters/monstersQueryOptions.ts
+++ src/queries/monsters/monstersQueryOptions.ts
@@ -1,8 +1,15 @@
 import { queryOptions } from "@tanstack/react-query";
 import queryConfig from "../queryConfig";
-import { fetchMonsterById } from "./monsters";
+import { fetchMonsterById, fetchMonsters } from "./monsters";
 
 export const monstersKey = "monsters";
+
+export const monstersQueryOptions = () =>
+  queryOptions({
+    queryKey: [monstersKey],
+    staleTime: queryConfig.staleTime,
+    queryFn: () => fetchMonsters(),
+  });
 
 export const monsterByIdQueryOptions = (id: number | string) =>
   queryOptions({
