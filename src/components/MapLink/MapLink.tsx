de16743ebf10b34e4196c56d1dae625c4ed49606
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-02 16:28:40 -0400

    Add map link that sets position when clicked

---
+++ src/components/MapLink/MapLink.tsx
@@ -0,0 +1,33 @@
+import { useMapControls } from "@/contexts/MapControls/useMapControls";
+import { cn } from "@/lib/utils";
+
+type MapLinkProps = {
+  x: number;
+  y: number;
+  z?: number;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const MapLink: React.FC<MapLinkProps> = ({
+  x,
+  y,
+  z,
+  children,
+  className = "",
+}) => {
+  const { setTargetPosition } = useMapControls();
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setTargetPosition({ x, y, z });
+  };
+
+  return (
+    <span onClick={handleClick} className={cn("cursor-pointer", className)}>
+      {children}
+    </span>
+  );
+};
+
+export default MapLink;
