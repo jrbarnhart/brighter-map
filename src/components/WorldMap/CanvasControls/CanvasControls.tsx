38e58c67617b25c1efecaaafef8c1351b6c56475
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-02 16:26:17 -0400

    Add effect that updates camera position on context change

--- src/components/WorldMap/CanvasControls/CanvasControls.tsx
+++ src/components/WorldMap/CanvasControls/CanvasControls.tsx
@@ -1,11 +1,35 @@
-import { MapControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
+import { MapControls } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { useMapControls } from "@/contexts/MapControls/useMapControls";
 
 export default function CanvasControls() {
+  const controlsRef = useRef<React.ComponentRef<typeof MapControls>>(null);
   const { invalidate } = useThree();
+  const { targetPosition } = useMapControls();
+
+  useEffect(() => {
+    if (targetPosition && controlsRef.current) {
+      controlsRef.current.target.set(targetPosition.x, targetPosition.y, 0);
+
+      if (targetPosition.z !== undefined) {
+        const camera = controlsRef.current.object;
+
+        camera.position.set(
+          targetPosition.x,
+          targetPosition.y,
+          targetPosition.z
+        );
+      }
+
+      controlsRef.current.update();
+      invalidate();
+    }
+  }, [invalidate, targetPosition]);
 
   return (
     <MapControls
+      ref={controlsRef}
       screenSpacePanning={true}
       panSpeed={1.5}
       zoomSpeed={1.5}
