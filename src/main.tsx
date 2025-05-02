b706e9b91b6dded14b0a3fe475714dc50a5496a8
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-02 15:50:05 -0400

    Add provider for map controls context

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import Router from "./routes/mainRouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MapControlsProvider } from "./contexts/MapControls/MapControlsProvider";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <MapControlsProvider>
+        <Router />
+      </MapControlsProvider>
     </QueryClientProvider>
   </StrictMode>
 );
