6d00cadd6d4d0f1d5d7b03c62358f07d4dae2184
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 01:40:03 -0400

    Make quest steps collapsible

--- src/components/InfoPanel/infoContents/InfoQuestSteps.tsx
+++ src/components/InfoPanel/infoContents/InfoQuestSteps.tsx
@@ -1,5 +1,12 @@
 import { components } from "@/lib/types/apiTypes";
 import InfoLabel from "./InfoLabel";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { useState } from "react";
+import { Eye, EyeClosed } from "lucide-react";
 
 type InfoQuestStepsProps = {
   data: {
@@ -8,22 +15,39 @@ type InfoQuestStepsProps = {
 };
 
 export default function InfoQuestSteps({ data }: InfoQuestStepsProps) {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       {data.questSteps.length > 0 && (
         <div>
-          <InfoLabel>Quest Steps:</InfoLabel>
-          <div className="grid gap-4">
-            {data.questSteps.map((step) => (
-              <p
-                key={`${step.questId.toString()}-${step.id.toString()}`}
-                className="font-bold flex flex-col"
+          <Collapsible open={open}>
+            <div className="flex items-center gap-2">
+              <InfoLabel>Quest Steps:</InfoLabel>
+              <CollapsibleTrigger
+                onClick={() => {
+                  setOpen((prev) => !prev);
+                }}
+                className="flex gap-1 bg-stone-700 hover:bg-stone-600 cursor-pointer p-1 rounded-md"
               >
-                {`${step.quest.name} #${step.index.toString()}:`}
-                <span className="font-normal">{step.description}</span>
-              </p>
-            ))}
-          </div>
+                {open ? "Hide" : "Show"}
+                {open ? <Eye /> : <EyeClosed />}
+              </CollapsibleTrigger>
+            </div>
+            <CollapsibleContent>
+              <div className="grid gap-4">
+                {data.questSteps.map((step) => (
+                  <p
+                    key={`${step.questId.toString()}-${step.id.toString()}`}
+                    className="font-bold flex flex-col"
+                  >
+                    {`${step.quest.name} #${step.index.toString()}:`}
+                    <span className="font-normal">{step.description}</span>
+                  </p>
+                ))}
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
         </div>
       )}
     </>
