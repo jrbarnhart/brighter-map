9bea866a32ebe8086d99b40b6993c45110c8cedb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 18:51:57 -0400

    Swap to data router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import Router from "./routes/mainRouter";
+import { router } from "./routes/mainRouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MapControlsProvider } from "./contexts/MapControls/MapControlsProvider";
+import { RouterProvider } from "react-router";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,7 @@ createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MapControlsProvider>
-        <Router />
+        <RouterProvider router={router} />
       </MapControlsProvider>
     </QueryClientProvider>
   </StrictMode>
--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
 import RootRoute from "./root/root.route";
 import RoomDetails from "@/routes/RoomDetails/RoomDetails";
 import MonsterDetails from "@/routes/MonsterDetails/MonsterDetails";
@@ -12,24 +16,20 @@ import ConsumableDetails from "./ConsumableDetails/ConsumableDetails";
 import WeaponDetails from "./WeaponDetails/WeaponDetails";
 import ArmorDetails from "./ArmorDetails/ArmorDetails";
 
-export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootRoute />}>
-          <Route index element={<InfoPanelIndex />} />
-          <Route path="/misc/:id" element={<MiscItemDetails />} />
-          <Route path="/consumables/:id" element={<ConsumableDetails />} />
-          <Route path="/resources/:id" element={<ResourceDetails />} />
-          <Route path="/weapons/:id" element={<WeaponDetails />} />
-          <Route path="/armors/:id" element={<ArmorDetails />} />
-          <Route path="/rooms/:id" element={<RoomDetails />} />
-          <Route path="/monsters/:id" element={<MonsterDetails />} />
-          <Route path="/vendors/:id" element={<VendorDetails />} />
-          <Route path="/npcs/:id" element={<NpcDetails />} />
-          <Route path="/quests/:id" element={<QuestDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootRoute />}>
+      <Route index element={<InfoPanelIndex />} />
+      <Route path="/misc/:id" element={<MiscItemDetails />} />
+      <Route path="/consumables/:id" element={<ConsumableDetails />} />
+      <Route path="/resources/:id" element={<ResourceDetails />} />
+      <Route path="/weapons/:id" element={<WeaponDetails />} />
+      <Route path="/armors/:id" element={<ArmorDetails />} />
+      <Route path="/rooms/:id" element={<RoomDetails />} />
+      <Route path="/monsters/:id" element={<MonsterDetails />} />
+      <Route path="/vendors/:id" element={<VendorDetails />} />
+      <Route path="/npcs/:id" element={<NpcDetails />} />
+      <Route path="/quests/:id" element={<QuestDetails />} />
+    </Route>
+  )
+);
