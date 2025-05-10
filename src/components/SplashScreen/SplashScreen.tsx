6c41f3a31e49657c2dfdb0baf6fbdda90d457fde
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 18:17:05 -0400

    Add simple splash screen component

---
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,8 @@
+export default function SplashScreen() {
+  return (
+    <div className="h-svh w-svw flex flex-col items-center">
+      <h1>Brighter Map</h1>
+      <p>Loading...</p>
+    </div>
+  );
+}
