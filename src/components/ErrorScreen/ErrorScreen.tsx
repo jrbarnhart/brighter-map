d1ff59d024f30b3019868cd8dbdb1f9dace8fdeb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 19:26:22 -0400

    Add link that reloads doc and goes home

--- src/components/ErrorScreen/ErrorScreen.tsx
+++ src/components/ErrorScreen/ErrorScreen.tsx
@@ -1,4 +1,4 @@
-import { isRouteErrorResponse, useRouteError } from "react-router";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router";
 
 type ErrorScreenProps = {
   error?: Error;
@@ -26,6 +26,13 @@ export default function ErrorScreen({ error }: ErrorScreenProps) {
       <div className="flex flex-col items-center justify-center gap-10 p-10 bg-stone-700 border border-red-400 rounded-2xl">
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg">{message}</p>
+        <Link
+          to={"/"}
+          className="text-xl text-sky-400 hover:brightness-125 hover:underline font-bold cursor-pointer"
+          reloadDocument={true}
+        >
+          Return Home
+        </Link>
       </div>
     </div>
   );
