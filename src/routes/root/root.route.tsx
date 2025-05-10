05fcd20a2211f6d4653be4555d6ae3c4b2a1f947
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 18:35:25 -0400

    Add splash screen when loading

--- src/routes/root/root.route.tsx
+++ src/routes/root/root.route.tsx
@@ -1,5 +1,6 @@
 import { FiltersState } from "@/components/FiltersPanel/FiltersPanel";
 import MapApp from "@/components/MapApp/MapApp";
+import SplashScreen from "@/components/SplashScreen/SplashScreen";
 
 // import useLazyQueries from "@/lib/hooks/useLazyQueries";
 import { baseMapDataQueryOptions } from "@/queries/baseMapData/baseMapDataQueryOptions";
@@ -39,11 +40,7 @@ function RootRoute() {
     setShowObelisk,
   };
 
-  // Lazy data loading
-  //const { data: lazyData, handlers: lazyHandlers } = useLazyQueries();
-
-  // TODO: Replace with loading interface
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <SplashScreen />;
 
   // TODO: Throw an error after error boundary established with react router
   if (isError || !data) return <p>Error!</p>;
