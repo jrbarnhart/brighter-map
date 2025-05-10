1d89ec208df6be2066644353b49f1593c513d0d6
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 17:15:22 -0400

    Add room link to quest steps

--- src/routes/QuestDetails/QuestDetails.tsx
+++ src/routes/QuestDetails/QuestDetails.tsx
@@ -2,15 +2,19 @@ import InfoContainer from "@/components/InfoPanel/infoContents/InfoContainer";
 import InfoLabel from "@/components/InfoPanel/infoContents/InfoLabel";
 import InfoSkeleton from "@/components/InfoPanel/infoContents/InfoSkeleton";
 import InfoTitle from "@/components/InfoPanel/infoContents/InfoTitle";
+import { InfoPanelContext } from "@/components/InfoPanel/InfoPanel";
+import MapLink from "@/components/MapLink/MapLink";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { questByIdQueryOptions } from "@/queries/quests/questsQueryOptions";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router";
+import { useOutletContext, useParams } from "react-router";
 
 export default function QuestDetails() {
   const { id } = useParams();
   const idNum = Number(id);
+  const context: InfoPanelContext | undefined = useOutletContext();
+  const { setInfoOpen, screenSize } = context || {};
 
   const { data, isLoading, error } = useQuery(questByIdQueryOptions(idNum));
 
@@ -46,18 +50,37 @@ export default function QuestDetails() {
             .map((step) => (
               <div
                 key={`step-${step.id.toString()}`}
-                className="grid grid-cols-[min-content_1fr] gap-3"
+                className="flex flex-col gap-1 border-b pb-2"
               >
-                <Checkbox
-                  id={`step-${step.index.toString()}-id:${step.id.toString()}`}
-                  className="mt-1"
-                />
-                <Label
-                  htmlFor={`step-${step.index.toString()}-id:${step.id.toString()}`}
-                  className="text-base"
-                >
-                  #{step.index} {step.description}
-                </Label>
+                {/* Checkbox + Label */}
+                <div className="flex items-start gap-3">
+                  <Checkbox
+                    id={`step-${step.index.toString()}-id:${step.id.toString()}`}
+                    className="mt-1"
+                  />
+                  <Label
+                    htmlFor={`step-${step.index.toString()}-id:${step.id.toString()}`}
+                    className="text-base"
+                  >
+                    #{step.index} {step.description}
+                  </Label>
+                </div>
+
+                {/* Optional Action Links */}
+                {step.roomId && (
+                  <div className="flex gap-2 pl-7 pt-1">
+                    {step.roomId && (
+                      <div className="flex items-center gap-2">
+                        <p>Room: </p>
+                        <MapLink
+                          roomId={step.roomId}
+                          setInfoOpen={setInfoOpen}
+                          closeInfo={screenSize && screenSize.width <= 768}
+                        />
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
             ))}
         </div>
