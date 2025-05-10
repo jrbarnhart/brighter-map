c11ffe51d60f7c71b657f97b5924cd4439c818b2
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-05-10 18:35:18 -0400

    Adjust style

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,8 +1,10 @@
 export default function SplashScreen() {
   return (
-    <div className="h-svh w-svw flex flex-col items-center">
-      <h1>Brighter Map</h1>
-      <p>Loading...</p>
+    <div className="grid content-center justify-center h-svh w-svw bg-stone-800 text-stone-50">
+      <div className="flex flex-col items-center justify-center gap-10 p-10 bg-stone-700 border border-amber-300 rounded-2xl">
+        <h1 className="text-2xl font-bold">Welcome to Brighter Map</h1>
+        <p className="italic">Loading map data...</p>
+      </div>
     </div>
   );
 }
